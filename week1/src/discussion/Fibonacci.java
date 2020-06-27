2900ab9c7df4e04c341cbb50eaf13ae7117b28a7
yiminliu <emily141@163.com>
2020-06-27 17:54:11 +0800

    homework

---
+++ week1/src/discussion/Fibonacci.java
@@ -0,0 +1,17 @@
+package discussion;
+
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else if (n == 0){
+            return 0;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+}
