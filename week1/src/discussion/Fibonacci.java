133f0ed12e45c2d207bda004c4e40ef4a9b63673
yiminliu <emily141@163.com>
2020-06-27 18:03:08 +0800

    homework

--- week1/src/discussion/Fibonacci.java
+++ week1/src/discussion/Fibonacci.java
@@ -2,10 +2,8 @@ package discussion;
 
 public class Fibonacci {
     public static int fib(int n) {
-        if (n == 1 || n == 2) {
-            return 1;
-        } else if (n == 0){
-            return 0;
+        if (n <= 2) {
+            return n;
         } else {
             return fib(n - 1) + fib(n - 2);
         }
